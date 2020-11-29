ac6ca427eb7835491d303a05b3b075870023dbf8
JeongWonjae <justv95@naver.com>
2020-11-29 11:34:36 +0900

    update .

--- app/config/database.php
+++ app/config/database.php
@@ -1,6 +1,6 @@
 <?php
-	$user="root";
-	$pw="root";
-	$host="localhost";
+	$user="adminuser@dbserverwj";
+	$pw="abcd123456789!";
+	$host="dbserverwj.mysql.database.azure.com";
 	$dbName="php_bank";
 ?>
